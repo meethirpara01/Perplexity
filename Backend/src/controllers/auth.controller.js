d413b09dbc34080d57d414ca6ce4e4c63e98ee43
Meet. Hirpara <meetkumarhirpara@gmail.com>
2026-04-01 16:46:54 +0530

    urls

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -38,7 +38,7 @@ export async function register(req, res) {
                 <p>Hi ${username},</p>
                 <p>Thank you for registering at <strong>Perplexity</strong>. We're excited to have you on board!</p>
                 <p>Please verify your email address by clicking the link below:</p>
-                <a href="http://localhost:3000/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
+                <a href="https://perplexity-islp.onrender.com/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
                 <p>If you did not create an account, please ignore this email.</p>
                 <p>Best regards,<br>The Perplexity Team</p>
         `
@@ -179,7 +179,7 @@ export async function resendVerificationEmail(req, res) {
                 <p>Hi ${user.username},</p>
                 <p>Thank you for registering at <strong>Perplexity</strong>. We're excited to have you on board!</p>
                 <p>Please verify your email address by clicking the link below:</p>
-                <a href="http://localhost:3000/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
+                <a href="https://perplexity-islp.onrender.com/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
                 <p>If you did not create an account, please ignore this email.</p>
                 <p>Best regards,<br>The Perplexity Team</p>
         `
@@ -212,7 +212,7 @@ export async function verifyEmail(req, res) {
             const html = `
                 <h3>Hey ${user.username},</h3>
                 <p>Your email is already verified. You can log in to your account.</p>
-                <a href="http://localhost:3000/login">Login to Perplexity</a>
+                <a href="https://perplexity-islp.onrender.com/login">Login to Perplexity</a>
                 <p>Best regards,<br/>The Perplexity Team</p>
             `
 
@@ -226,7 +226,7 @@ export async function verifyEmail(req, res) {
                 <h3>Hey ${user.username},</h3>
                 <h3>Email Verified Successfully!</h3>
                 <p>Your email has been verified. You can now log in to your account.</p>
-                <a href="http://localhost:3000/login">Login to Perplexity</a>
+                <a href="https://perplexity-islp.onrender.com/login">Login to Perplexity</a>
                 <p>Best regards,<br/>The Perplexity Team</p>
         `
 
